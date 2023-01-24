bd1c4f8a08ec99b01dc35664ab2e10ce10a09eb5
SinshiSmith <yorfnd@gmail.com>
2023-01-24 05:43:47 +0200

    Refacotr get_champions to use anyhow

--- src/routes/get_champions.rs
+++ src/routes/get_champions.rs
@@ -5,6 +5,7 @@ use serde::Serialize;
 
 use crate::database::champion;
 use crate::database::champion_star;
+use crate::errors::AppError;
 
 #[derive(Clone, Serialize)]
 pub struct ResponseChampion {
@@ -16,25 +17,19 @@ pub struct ResponseChampion {
 
 pub async fn get_champions(
     Extension(database): Extension<DatabaseConnection>,
-) -> impl IntoResponse {
-    match champion::Entity::find()
-        .find_with_related(champion_star::Entity)
-        .all(&database)
-        .await
-    {
-        Ok(champions) => {
-            let champions_list = champions
-                .iter()
-                .map(|(champion, stars)| ResponseChampion {
-                    id: champion.id.clone(),
-                    name: champion.name.clone(),
-                    cost: champion.cost,
-                    damage: stars.iter().map(|star| star.damage).collect(),
-                })
-                .collect::<Vec<ResponseChampion>>();
-
-            (StatusCode::OK, Ok(Json(champions_list)))
-        }
-        Err(_) => (StatusCode::NOT_FOUND, Err("Error while getting champions")),
-    }
+) -> Result<Json<Vec<ResponseChampion>>, AppError> {
+    Ok(Json(
+        champion::Entity::find()
+            .find_with_related(champion_star::Entity)
+            .all(&database)
+            .await?
+            .iter()
+            .map(|(champion, stars)| ResponseChampion {
+                id: champion.id.clone(),
+                name: champion.name.clone(),
+                cost: champion.cost,
+                damage: stars.iter().map(|star| star.damage).collect(),
+            })
+            .collect::<Vec<ResponseChampion>>(),
+    ))
 }
