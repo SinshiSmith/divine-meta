46d0688624af0d3a7f4b52f60849718b11f47a3f
SinshiSmith <yorfnd@gmail.com>
2023-01-24 12:44:03 +0200

    create /champions/:id route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,11 +4,12 @@ use axum::{body::Body, routing::get, Extension, Router};
 use sea_orm::DatabaseConnection;
 
 use axum;
-use get_champions::get_champions;
+use get_champions::{get_champions, get_one_champion};
 
 pub fn create_routes(database: DatabaseConnection) -> Router<(), Body> {
     Router::new()
         .route("/", get(|| async { "Hello World" }))
         .route("/champions", get(get_champions))
+        .route("/champions/:id", get(get_one_champion))
         .layer(Extension(database))
 }
