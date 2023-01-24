4656e3fd4424ddeaf256730d03f50dcee084f0dc
SinshiSmith <yorfnd@gmail.com>
2023-01-24 12:45:52 +0200

    Create /champions/:id Endpoint (#3)
    * Refactor AppError to accept custom errors
    
    * Send full champion data:
    - Refactor ResponseChampion to Champion
    - impl new for champion to send full data
    - Refactor get_champions to use new struct
    
    * Create get_one_champion handler
    
    * create /champions/:id route

--- src/routes/get_champions.rs
+++ src/routes/get_champions.rs
@@ -1,5 +1,8 @@
+use axum::extract::Path;
+use axum::http::StatusCode;
+use axum::Extension;
 use axum::Json;
-use axum::{http::StatusCode, response::IntoResponse, Extension};
+use sea_orm::ModelTrait;
 use sea_orm::{DatabaseConnection, EntityTrait};
 use serde::Serialize;
 
@@ -8,28 +11,75 @@ use crate::database::champion_star;
 use crate::errors::AppError;
 
 #[derive(Clone, Serialize)]
-pub struct ResponseChampion {
+pub struct Champion {
     id: String,
     name: String,
     cost: i32,
+    mana: i32,
+    starting_mana: i32,
+    armor: i32,
+    magic_resist: i32,
+    attack_speed: f32,
+    crit_rate: i32,
+    range: i32,
     damage: Vec<i32>,
+    health: Vec<i32>,
+}
+
+impl Champion {
+    fn new(basic: &champion::Model, stars: &Vec<champion_star::Model>) -> Self {
+        let (damage, health) =
+            stars
+                .iter()
+                .fold((vec![], vec![]), |(mut damage, mut health), star| {
+                    damage.push(star.damage);
+                    health.push(star.health);
+                    (damage, health)
+                });
+        Self {
+            damage,
+            health,
+            id: basic.id.clone(),
+            name: basic.name.clone(),
+            cost: basic.cost,
+            mana: basic.mana,
+            starting_mana: basic.starting_mana,
+            armor: basic.armor,
+            magic_resist: basic.magic_resist,
+            attack_speed: basic.attack_speed,
+            crit_rate: basic.crit_rate,
+            range: basic.range,
+        }
+    }
 }
 
 pub async fn get_champions(
     Extension(database): Extension<DatabaseConnection>,
-) -> Result<Json<Vec<ResponseChampion>>, AppError> {
+) -> Result<Json<Vec<Champion>>, AppError> {
     Ok(Json(
         champion::Entity::find()
             .find_with_related(champion_star::Entity)
             .all(&database)
             .await?
             .iter()
-            .map(|(champion, stars)| ResponseChampion {
-                id: champion.id.clone(),
-                name: champion.name.clone(),
-                cost: champion.cost,
-                damage: stars.iter().map(|star| star.damage).collect(),
-            })
-            .collect::<Vec<ResponseChampion>>(),
+            .map(|(champion, stars)| Champion::new(champion, stars))
+            .collect::<Vec<Champion>>(),
     ))
+}
+
+pub async fn get_one_champion(
+    Extension(database): Extension<DatabaseConnection>,
+    Path(id): Path<String>,
+) -> Result<Json<Champion>, AppError> {
+    match champion::Entity::find_by_id(id).one(&database).await? {
+        Some(champion) => {
+            let stars = champion
+                .find_related(champion_star::Entity)
+                .all(&database)
+                .await?;
+
+            Ok(Json(Champion::new(&champion, &stars)))
+        }
+        None => Err(AppError::new(StatusCode::NOT_FOUND, "Champion Not Found")),
+    }
 }
