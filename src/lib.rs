9ed9e63740d26721b5476d483f0c596b041295ea
SinshiSmith <yorfnd@gmail.com>
2023-05-08 22:24:23 +0300

    run app on port 3010

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub async fn app(database_uri: &str) {
     let database = Database::connect(database_uri).await.unwrap();
     let app = create_routes(database);
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:3010".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap()
